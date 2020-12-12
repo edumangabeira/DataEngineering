ee1321221c957d20747469125365f0f44ae7e6ca
edumangabeira <edu.mangaba@gmail.com>
2020-12-11 23:55:43 -0300

    terminei aggregations

--- udacity/agreggations.sql
+++ udacity/agreggations.sql
@@ -75,7 +75,6 @@ ORDER BY a.total_amt_usd
 -- Não consegui pensar num jeito de capturar as posições.
 
 
-
 -- 14.1
 
 SELECT
@@ -158,4 +157,217 @@ GROUP BY e.name
 ORDER BY sales_rep_qty;
 
 
+-- 17.1
 
+SELECT
+a.name,
+AVG(b.standard_qty) AS avg_std_qty,
+AVG(b.gloss_qty) AS avg_gloss_qty,
+AVG(b.poster_qty) AS avg_poster_qty
+FROM orders b
+JOIN accounts a
+ON b.account_id = a.id
+GROUP BY a.name;
+
+-- 17.2
+
+SELECT
+a.name,
+AVG(b.standard_amt_usd) AS avg_std_amt_usd,
+AVG(b.gloss_amt_usd) AS avg_gloss_amt_usd,
+AVG(b.poster_amt_usd) AS avg_poster_amt_usd
+FROM orders b
+JOIN accounts a
+ON b.account_id = a.id
+GROUP BY a.name;
+
+-- 17.3
+
+SELECT
+COUNT(a.channel) as channel_count,
+a.channel,
+c.name as sales_rep_name
+FROM web_events a
+JOIN accounts b
+ON a.account_id = b.id
+JOIN sales_reps c
+ON b.sales_rep_id = c.id
+GROUP BY a.channel, sales_rep_name
+ORDER BY channel_count DESC;
+
+-- 17.4
+
+SELECT
+COUNT(a.channel) as channel_count,
+a.channel,
+d.name as region_name
+FROM web_events a
+JOIN accounts b
+ON a.account_id = b.id
+JOIN sales_reps c
+ON b.sales_rep_id = c.id
+JOIN region d
+ON c.region_id = d.id
+GROUP BY a.channel, region_name
+ORDER BY channel_count DESC;
+
+-- 20.1
+
+SELECT a.name AS account, c.name AS region
+FROM accounts a
+JOIN sales_reps b
+ON a.sales_rep_id = b.id
+JOIN region c
+ON b.region_id = c.id;
+
+SELECT DISTINCT id, name
+FROM accounts;
+-- 20.2
+
+SELECT a.name AS account, b.name AS sales_rep, COUNT(b.name) AS count_sales_rep
+FROM accounts a
+JOIN sales_reps b
+ON a.sales_rep_id = b.id
+GROUP BY account, sales_rep
+ORDER BY count_sales_rep DESC;
+
+SELECT DISTINCT id, name
+FROM sales_reps;
+
+-- 23.1
+
+SELECT b.name AS sales_rep,
+COUNT(a.name) AS accounts
+FROM accounts a
+JOIN sales_reps b
+ON a.sales_rep_id = b.id
+GROUP BY sales_rep
+HAVING COUNT(a.name) > 5
+
+--23.2
+
+SELECT COUNT(c.id) AS orders,
+a.id AS accounts
+FROM accounts a
+JOIN orders c
+ON a.id = c.account_id
+GROUP BY accounts
+HAVING COUNT(c.id) > 20
+ORDER BY orders
+
+--23.3
+
+SELECT COUNT(c.id) AS orders,
+a.name AS accounts
+FROM accounts a
+JOIN orders c
+ON a.id = c.account_id
+GROUP BY accounts
+HAVING COUNT(c.id) > 20
+ORDER BY orders DESC
+LIMIT 1
+
+--23.4
+
+SELECT SUM(c.total_amt_usd) AS orders_total_usd,
+a.name AS accounts
+FROM accounts a
+JOIN orders c
+ON a.id = c.account_id
+GROUP BY accounts
+HAVING SUM(c.total_amt_usd) > 30000
+
+
+--23.5
+
+SELECT SUM(c.total_amt_usd) AS orders_total_usd,
+a.name AS accounts
+FROM accounts a
+JOIN orders c
+ON a.id = c.account_id
+GROUP BY accounts
+HAVING SUM(c.total_amt_usd) < 1000
+
+--23.6
+
+SELECT SUM(c.total_amt_usd) AS orders_total_usd,
+a.name AS accounts
+FROM accounts a
+JOIN orders c
+ON a.id = c.account_id
+GROUP BY accounts
+ORDER BY orders_total_usd DESC
+LIMIT 1
+
+--23.7
+
+SELECT SUM(c.total_amt_usd) AS orders_total_usd,
+a.name AS accounts
+FROM accounts a
+JOIN orders c
+ON a.id = c.account_id
+GROUP BY accounts
+ORDER BY orders_total_usd
+LIMIT 1
+
+--23.8
+
+SELECT COUNT(b.channel),
+a.name
+FROM web_events b
+JOIN accounts a
+ON b.account_id = a.id
+AND b.channel LIKE 'facebook'
+GROUP BY a.name
+HAVING COUNT(b.channel) > 6
+
+--23.9
+
+SELECT COUNT(b.channel) AS times_contact,
+a.name
+FROM web_events b
+JOIN accounts a
+ON b.account_id = a.id
+AND b.channel LIKE 'facebook'
+GROUP BY a.name
+ORDER BY times_contact DESC
+LIMIT 1
+
+--23.10
+
+SELECT COUNT(b.channel) AS times_contact,
+a.name,
+b.channel
+FROM web_events b
+JOIN accounts a
+ON b.account_id = a.id
+GROUP BY a.name, b.channel
+ORDER BY times_contact DESC
+LIMIT 15
+
+
+-- 27.1
+
+SELECT DATE_TRUNC('year',occurred_at), SUM(total_amt_usd) as total
+FROM orders
+GROUP BY DATE_TRUNC('year', occurred_at)
+ORDER BY DATE_TRUNC('year', occurred_at)
+
+-- Entre 2013 e 2016 parece haver crescimento anual nas vendas, mas em 2017 há uma queda brusca.
+-- Os dados de 2013 e 2017 não estão completos.
+
+
+-- 31.5
+
+SELECT
+COUNT(a.id) as count_orders,
+b.name as sales_rep,
+CASE WHEN COUNT(a.id) > 200 THEN 'top'
+     ELSE 'not top' END AS sales_level
+FROM orders a
+JOIN accounts c
+ON a.account_id = c.id
+JOIN sales_reps b
+ON c.sales_rep_id = b.id
+GROUP BY sales_rep
+ORDER BY count_orders DESC
